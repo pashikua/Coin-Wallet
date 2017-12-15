be7abe7f25269241294e8d802e651a2d467b935f
Özgür C <oezguercelebi@gmail.com>
2017-12-15 16:36:19 +0100

    color extensions

---
+++ Coin Wallet/Extensions/UIColorExtensions.swift
@@ -0,0 +1,20 @@
+//
+//  UIColorExtensions.swift
+//  Coin Wallet
+//
+//  Created by Özgür Celebi on 15.12.2017.
+//  Copyright © 2017 Özgür Celebi. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    // Primary Color = #1EA1D3
+    static let primaryColor = UIColor(red:0.12, green:0.63, blue:0.83, alpha:1.0)
+    
+    // Danger Color = #FF1949
+    static let dangerColor = UIColor(red:1.0, green:0.1, blue:0.29, alpha:1.0)
+    
+    // Light Dark Text Color = #394149
+    static let lightDarkColor = UIColor(red:0.22, green:0.25, blue:0.29, alpha:1.0)
+}
