fa0437ad600271895d1df04166b02f22e3f34219
Özgür C <oezguercelebi@gmail.com>
2017-12-15 16:37:31 +0100

    add swipe delegate for cells

--- Coin Wallet/Controllers/CoinDataService.swift
+++ Coin Wallet/Controllers/CoinDataService.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import SwipeCellKit
+import SCLAlertView
 
-class CoinDataService: NSObject, UITableViewDataSource, UITableViewDelegate {
+class CoinDataService: NSObject, UITableViewDataSource, UITableViewDelegate, SwipeTableViewCellDelegate {
     
     var coinManager: CoinManager?
     
@@ -23,6 +25,7 @@ class CoinDataService: NSObject, UITableViewDataSource, UITableViewDelegate {
         
         let coin = coinManager.coinAtIndex(index: indexPath.row)
         
+        cell.delegate = self
         cell.configCoinCell(withCoin: coin)
         
         return cell
@@ -31,5 +34,58 @@ class CoinDataService: NSObject, UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // TODO: show detailed view
         print("Did select row at:", indexPath.row)
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> [SwipeAction]? {
+        
+        let editAction = SwipeAction(style: .default, title: "Edit") { action, indexPath in
+            // handle action by updating model with edit
+            
+            // Customize
+            let appearance = SCLAlertView.SCLAppearance(
+                kTitleFont: UIFont(name: "HelveticaNeue", size: 19)!,
+                kTextFont: UIFont(name: "HelveticaNeue", size: 14)!,
+                kButtonFont: UIFont(name: "HelveticaNeue-Bold", size: 14)!,
+                showCloseButton: false
+            )
+            let alert = SCLAlertView(appearance: appearance)
+            let valueInString = String(describing: self.coinManager!.coinAtIndex(index: indexPath.row).holding!)
+            let valueTextField = alert.addTextField(valueInString)
+            valueTextField.keyboardType = .decimalPad
+            alert.addButton("Done") {
+                // Todo: check if value is correct and save to current index coin
+                // plus update the coin disk values, then reload current tableview data
+                print("Finished updating value: ", valueTextField.text!)
+            }
+            
+            alert.showCustom("Edit", subTitle: "Update your holding value", color: .primaryColor, icon: UIImage(named: "editFilled")!)
+            
+            
+            tableView.reloadData()
+        }
+        // customize the action appearance
+        editAction.image = UIImage(named: "editRoundFilled")
+        editAction.backgroundColor = .white
+        editAction.textColor = .lightDarkColor
+        
+        
+        let deleteAction = SwipeAction(style: .destructive, title: "Delete") { action, indexPath in
+            // handle action by updating model with deletion
+            self.coinManager?.removeCoinFromLibrary(index: indexPath.row)
+            
+            tableView.reloadData()
+        }
+        // customize the action appearance
+        deleteAction.image = UIImage(named: "deleteRoundFilled")
+        deleteAction.backgroundColor = .white
+        deleteAction.textColor = .lightDarkColor
+        
+        return [editAction, deleteAction]
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsOptionsForRowAt indexPath: IndexPath, for orientation: SwipeActionsOrientation) -> SwipeTableOptions {
+        var options = SwipeTableOptions()
+        options.transitionStyle = .border
+        return options
     }
 }
