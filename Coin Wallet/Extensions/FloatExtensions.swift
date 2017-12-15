095cbb43037035e8f9392db46b62bf14bda8ee5e
Özgür C <oezguercelebi@gmail.com>
2017-12-15 02:36:17 +0100

    convert to currency us dollar

---
+++ Coin Wallet/Extensions/FloatExtensions.swift
@@ -0,0 +1,22 @@
+//
+//  FloatExtensions.swift
+//  Coin Wallet
+//
+//  Created by Özgür Celebi on 15.12.2017.
+//  Copyright © 2017 Özgür Celebi. All rights reserved.
+//
+
+import Foundation
+
+extension Float {
+    func changeToDollarCurrencyString() -> String {
+        let number = NSNumber(value: self)
+        let currencyFormatter = NumberFormatter()
+        currencyFormatter.usesGroupingSeparator = true
+        currencyFormatter.numberStyle = NumberFormatter.Style.currency
+        // localize to your grouping and decimal separator
+        currencyFormatter.locale = Locale(identifier: "en_US")
+        let priceString = currencyFormatter.string(from: number)
+        return priceString!
+    }
+}
