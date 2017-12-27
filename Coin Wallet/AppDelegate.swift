5f289ab0522e5513d114c47e8e71d60740366e59
Özgür C <oezguercelebi@gmail.com>
2017-12-27 16:29:51 +0100

    clear

--- Coin Wallet/AppDelegate.swift
+++ Coin Wallet/AppDelegate.swift
@@ -11,8 +11,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        
+    
 //        Fabric.sharedSDK().debug = true
         Fabric.with([Crashlytics.self])
 //        Crashlytics.sharedInstance().crash()
