7a7948870a9697f6b7f1eace6fc8f594f629c96d
Özgür C <oezguercelebi@gmail.com>
2017-12-28 16:53:07 +0100

    update pods

--- Coin Wallet/Controllers/SupportDeveloperViewController.swift
+++ Coin Wallet/Controllers/SupportDeveloperViewController.swift
@@ -2,23 +2,13 @@
 //  Copyright © 2017 Özgür Celebi. All rights reserved.
 
 import UIKit
+import StoreKit
 
 class SupportDeveloperViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.navigationItem.title = "Support Developer"
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
