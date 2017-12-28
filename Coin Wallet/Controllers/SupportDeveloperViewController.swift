d52ef3bac3fd68d573f95935e06f603c9e75eba0
Özgür C <oezguercelebi@gmail.com>
2017-12-28 16:20:30 +0100

    prepare support developer screen

---
+++ Coin Wallet/Controllers/SupportDeveloperViewController.swift
@@ -0,0 +1,24 @@
+//  Created by Özgür Celebi on 28.12.2017.
+//  Copyright © 2017 Özgür Celebi. All rights reserved.
+
+import UIKit
+
+class SupportDeveloperViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationItem.title = "Support Developer"
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
