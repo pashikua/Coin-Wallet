ae73ce692cea3eddc82c1ea4514e5370a7f58dbf
Özgür C <oezguercelebi@gmail.com>
2018-01-02 12:58:52 +0100

    first commit

---
+++ Coin Wallet/Controllers/EmptyViewController.swift
@@ -0,0 +1,13 @@
+//  Created by Özgür Celebi on 29.12.2017.
+//  Copyright © 2017 Özgür Celebi. All rights reserved.
+
+import UIKit
+
+class EmptyViewController: UIViewController {
+
+    @IBOutlet weak var infoLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
