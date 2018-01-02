889e10f54b62fc28b3e3dab6ea747d19bdb59024
Özgür C <oezguercelebi@gmail.com>
2018-01-02 12:54:54 +0100

    Add touch id checker and SwiftyStorekit completion

--- Coin Wallet/AppDelegate.swift
+++ Coin Wallet/AppDelegate.swift
@@ -4,6 +4,8 @@
 import UIKit
 import Fabric
 import Crashlytics
+import SwiftyStoreKit
+import LocalAuthentication
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 //        Fabric.sharedSDK().debug = true
         Fabric.with([Crashlytics.self])
 //        Crashlytics.sharedInstance().crash()
+        
+        completeIAPTransactions()
+        
+        checkForTouchID()
         
         return true
     }
@@ -40,7 +46,100 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    
+    // Mark: - TouchID
+    
+    func checkForTouchID() {
+        if UserDefaults.standard.bool(forKey: "isTouchIDEnabled") {
+            // Check for fingerprint
+            print("check for fingerprint")
+            // Send user to Empty View Controlller while checking for TouchID
+            
+            let storyboard = UIStoryboard.init(name: "Empty", bundle: nil)
+            let viewController = storyboard.instantiateViewController(withIdentifier: "EmptyVC") as! EmptyViewController
+            self.window?.rootViewController = viewController
+            self.window?.makeKeyAndVisible()
+            
+            let authContext = LAContext()
+            let authReason = "You are turning on Biometric Securiy to lock your Coin Portfolio"
+            
+            authContext.evaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, localizedReason: authReason, reply: { (success, error) in
+                
+                if success {
+                    // Verified Successfully
+                    DispatchQueue.main.async {
+                        let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
+                        let viewController = storyboard.instantiateViewController(withIdentifier: "NavigationVC") as! NavigationViewController
+                        
+                        self.window?.rootViewController = viewController
+                        self.window?.makeKeyAndVisible()
+                    }
+                } else {
+                    print("Authentification Failed")
+                    // Failed to Verification
+                    DispatchQueue.main.async {
+                        viewController.infoLabel.text = self.displayErrorMessage(error: error as! LAError)
+                        viewController.infoLabel.isHidden = false
+                    }
+                }
+            })
+        } else {
+            print("Touch ID is not enabled")
+            // Let user through since TouchID is not enabled
+            DispatchQueue.main.async {
+                let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
+                let viewController = storyboard.instantiateViewController(withIdentifier: "NavigationVC") as! NavigationViewController
+                
+                self.window?.rootViewController = viewController
+                self.window?.makeKeyAndVisible()
+            }
+        }
+    }
+    
+    func displayErrorMessage(error: LAError) -> String {
+        var message = ""
+        switch error.code {
+        case LAError.authenticationFailed:
+            message = "Authentication was not successful because the user failed to provide valid credentials."
+            break
+        case LAError.userCancel:
+            message = "Authentication was canceled by the user"
+            break
+        case LAError.userFallback:
+            message = "Authentication was canceled because the user tapped the fallback button"
+            break
+        case LAError.systemCancel:
+            message = "Authentication was canceled by system"
+            break
+        case LAError.biometryNotEnrolled:
+            message = "Biometry Authentication is enabled for Coin Portfolio. Until you haven't enabled FaceID or TouchID you can't get back in."
+            break
+        default:
+            message = error.localizedDescription
+        }
+        
+        return message
+    }
+    
+    // Mark: - SwiftyStoreKit
 
-
+    func completeIAPTransactions() {
+        SwiftyStoreKit.completeTransactions(atomically: true) { purchases in
+            for purchase in purchases {
+                switch purchase.transaction.transactionState {
+                case .purchased, .restored:
+                    if purchase.needsFinishTransaction {
+                        // Deliver content from server, then:
+                        SwiftyStoreKit.finishTransaction(purchase.transaction)
+                    }
+                    print("\(purchase.transaction.transactionState.debugDescription): \(purchase.productId)")
+                // Unlock content
+                case .failed, .purchasing, .deferred:
+                    break // do nothing
+                }
+            }
+        }
+    }
 }
 
