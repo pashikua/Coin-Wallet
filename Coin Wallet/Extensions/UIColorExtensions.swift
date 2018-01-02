d299d6bdf30680ab3bd73fa3d4152a7d2daface7
Özgür C <oezguercelebi@gmail.com>
2018-01-02 12:56:40 +0100

    change colors

--- Coin Wallet/Extensions/UIColorExtensions.swift
+++ Coin Wallet/Extensions/UIColorExtensions.swift
@@ -4,12 +4,12 @@
 import UIKit
 
 extension UIColor {
-    // Primary Color = #1EA1D3
-    static let primaryColor = UIColor(red:0.12, green:0.63, blue:0.83, alpha:1.0)
+    // Primary Default Color = #007AFF
+    static let primaryColor = UIColor(red:0.0, green:0.48, blue:1.0, alpha:1.0)
     
     // Danger Color = #FF1949
     static let dangerColor = UIColor(red:1.0, green:0.1, blue:0.29, alpha:1.0)
     
-    // Light Dark Text Color = #394149
-    static let lightDarkColor = UIColor(red:0.22, green:0.25, blue:0.29, alpha:1.0)
+    // Mid Dark Text Color = #394149
+    static let midDarkColor = UIColor(red:0.22, green:0.25, blue:0.29, alpha:1.0)
 }
