fd4fbe237f8ad0c93dc13a9667c92459f3b27120
Özgür C <oezguercelebi@gmail.com>
2018-01-02 12:58:37 +0100

    first commit

---
+++ Coin Wallet/Controllers/NavigationViewController.swift
@@ -0,0 +1,16 @@
+//  Created by Özgür Celebi on 29.12.2017.
+//  Copyright © 2017 Özgür Celebi. All rights reserved.
+
+import UIKit
+
+class NavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Show status bar
+        UIApplication.shared.isStatusBarHidden = false
+    }
+    
+    
+}
